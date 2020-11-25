a13e51b29bfcdfa5813a3c35adaf7af420667760
KlaasHagge <klaash@ppedv.de>
2020-11-25 16:29:26 +0100

    Tag 2: XAML, Layouts, NavigationPages (kommentiert)

--- X_Forms/X_Forms/App.xaml.cs
+++ X_Forms/X_Forms/App.xaml.cs
@@ -4,25 +4,38 @@ using Xamarin.Forms.Xaml;
 
 namespace X_Forms
 {
+    //Die App-Klasse beinhaltet eine grundlegen Initialisierung der App sowie die MainPage-Property, welche defniert,
+    //welche Page gerade in der App angezeigt wird. Diese Property wird auch als Einstiegspunkt verwendet.
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            //Zuweisung der MainPage-Property zu einer Page
+            //MainPage = new MainPage();
+
+            //Zuweisung der MainPage - Property zu einer NavigationPage(ermöglicht Stack - Navigation) mit Angabe der Startpage.
+            MainPage = new NavigationPage(new MainPage());
+
         }
 
+        public DateTime TimeStamp { get; set; }
+
+        //Methoden, welche zu bestimmten globalen Events ausgeführt werden (Start, Unterbrechen der App [Sleep], Wiederaktivierung der App [Resume])
         protected override void OnStart()
         {
+            (MainPage as NavigationPage).CurrentPage.DisplayAlert("Time", $"Startzeit: {DateTime.Now}", "Ok");
         }
 
         protected override void OnSleep()
         {
+            TimeStamp = DateTime.Now;
         }
 
         protected override void OnResume()
         {
+            (MainPage as NavigationPage).CurrentPage.DisplayAlert("Time", $"Geschlafene Zeit: {DateTime.Now.Subtract(TimeStamp).TotalSeconds}", "Ok");
         }
     }
 }
--- X_Forms/X_Forms/MainPage.xaml.cs
+++ X_Forms/X_Forms/MainPage.xaml.cs
@@ -11,28 +11,39 @@ namespace X_Forms
 {
     public partial class MainPage : ContentPage
     {
+        //Property zum Zwischenspeichern der Personenliste (ObservableCollection ist eine generische Liste, welche die GUI
+        //automatisch über Veränderungen informiert
         public ObservableCollection<string> NamensListe { get; set; }
 
+        //Konstruktor
         public MainPage()
         {
+            //Initialisierung der UI (Xaml-Datei). Sollte immer erste Aktion des Konstruktors sein
             InitializeComponent();
 
+            //Zuweisung von EventHandlern zu den Completed-Events, damit ein besserer Bedienfluss gegeben ist
             Ent_Vorname.Completed += (s, e) => Ent_Nachname.Focus();
             Ent_Nachname.Completed += Btn_Show_Clicked;
 
+            //Initialisierung der Namesliste
             NamensListe = new ObservableCollection<string>()
             {
                 "Anna Nass",
                 "Rainer Zufall"
             };
 
+            //Durch Setzen des BindingContextes nehmen Kurzbindungen aus dem XAML-Code automatisch Bezug auf die Properties
+            //des im BindingContext gesetzten Objekts
             this.BindingContext = this;
         }
 
+        //EventHandler
         private async void Btn_KlickMich_Clicked(object sender, EventArgs e)
         {
+            //Neuzuweisung einer UI-Property über die x:Name-Property des Steuerelements
             Lbl_Main.Text = "Danke, dass du auf den Button geklickt hast.";
 
+            //Anzeige einer 'MessageBox' und Abfrage der User-Antwort
             bool erg = await DisplayAlert("Begrüßung", $"Hallo, {Ent_Vorname.Text} {Ent_Nachname.Text}. Geht es dir gut?", "Ja", "Nein");
 
             if (erg)
@@ -44,14 +55,35 @@ namespace X_Forms
         private void Btn_Show_Clicked(object sender, EventArgs e)
         {
             if (Ent_Vorname.Text != String.Empty && Ent_Nachname.Text != String.Empty)
+
+                //Erstellen eines neuen Listenelements (aus UI-Properties)
                 NamensListe.Add(Ent_Vorname.Text + " " + Ent_Nachname.Text);
         }
 
-        private void MenuItem_Clicked(object sender, EventArgs e)
+        private async void MenuItem_Clicked(object sender, EventArgs e)
         {
-            string person = (sender as MenuItem).CommandParameter.ToString();
+            //Anzeige einer 'MessageBox' und Abfrage der User-Antwort
+            bool result = await DisplayAlert("Löschung", "Soll diese Person wirklich gelöscht werden?", "Ja", "Nein");
 
-            NamensListe.Remove(person);
+            if (result)
+            {
+                //Löschen eines Listeneintrags
+                string person = (sender as MenuItem).CommandParameter.ToString();
+
+                NamensListe.Remove(person);
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //Aufruf einer neuen Seite innerhalb der aktuellen NavigationPage 
+            Navigation.PushAsync(new Layouts.StackLay());
+        }
+    
+        private void Button2_Clicked(object sender, EventArgs e)
+        {
+            //Aufruf einer neuen Seite innerhalb der aktuellen NavigationPage, welche aber keinen 'Zurück'-Button anzeigt
+            Navigation.PushModalAsync(new Layouts.GridLay());
         }
     }
 }
